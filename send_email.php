0481b1a67004a46eeef56ac9009c55d02d75db76
Justin030 <55232645+Justin030@users.noreply.github.com>
2021-03-27 14:51:58 +0100

    php

--- send_email.php
+++ send_email.php
@@ -5,29 +5,14 @@ $name_von_emailfeld = "Email";   //Feld in der die Absenderadresse steht
 
 $empfaenger = "justin.bauer@espas.ch"; //Empfänger-Adresse
 $mail_cc = ""; //CC-Adresse, diese E-Mail-Adresse bekommt einer weitere Kopie
-$betreff = "Neue Kontaktanfrage"; //Betreff der Email
- 
+$betreff = "Neue Kontaktanfrage"; //Betreff der Email 
 $url_ok = "https://google.ch"; //Zielseite, wenn E-Mail erfolgreich versendet wurde
 $url_fehler = "https://espas.ch"; //Zielseite, wenn E-Mail nicht gesendet werden konnte
  
  
 //Diese Felder werden nicht in der Mail stehen
 $ignore_fields = array('submit');
- 
- 
- 
- 
-//Datum, wann die Mail erstellt wurde
-$name_tag = array("Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag");
-$num_tag = date("w");
-$tag = $name_tag[$num_tag];
-$jahr = date("Y");
-$n = date("d");
-$monat = date("m");
-$time = date("H:i");
- 
-//Erste Zeile unserer Email
-$msg = ":: Gesendet am $tag, den $n.$monat.$jahr - $time Uhr ::\n\n";
+
  
 //Hier werden alle Eingabefelder abgefragt
 foreach($_POST as $name => $value) {
