a1abdd2ac4f31c03d8f264b44010adb0ad92d0e7
Justin030 <55232645+Justin030@users.noreply.github.com>
2021-03-28 16:44:43 +0200

    gerhg

--- php/send_email.php
+++ php/send_email.php
@@ -36,15 +36,15 @@ $mail_senden = mail($empfaenger,$betreff,$msg,$header);
  
 //Weiterleitung, hier konnte jetzt per echo auch Ausgaben stehen
 if($mail_senden){
-  //header("Location: ".$url_ok); //Mail wurde gesendet
-  echo '<script type="text/javascript" language="Javascript"> alert("Vielen Dank! Ihre Daten wurden uns zugesandt.") </script> ';
   header("Location: ".$url_ok); //Mail wurde gesendet
+  echo '<script type="text/javascript" language="Javascript"> alert("Vielen Dank! Ihre Daten wurden uns zugesandt.") </script> ';
+  //header("Location: ".$url_ok); //Mail wurde gesendet
   exit();
   
 } else{
-  //header("Location: ".$url_fehler); //Fehler beim Senden
-  echo '<script type="text/javascript" language="Javascript"> alert("Nein") </script> ';
   header("Location: ".$url_fehler); //Fehler beim Senden
+  echo '<script type="text/javascript" language="Javascript"> alert("Nein") </script> ';
+  //header("Location: ".$url_fehler); //Fehler beim Senden
   exit();
   
 }
